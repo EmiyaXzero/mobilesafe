968e65a19e09466489f0d83935ef5e6527c2df21
Emiya_zero <a825564615@gmail.com>
2016-01-30 13:18:06 +0800

    新增网络连接功能

--- app/src/main/java/com/example/mobilesafe/SplashActivity.java
+++ app/src/main/java/com/example/mobilesafe/SplashActivity.java
@@ -4,7 +4,18 @@ import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.example.mobilesafe.com.example.mobilesafe.utils.StreamTool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Splash界面展示产品logo 应用初始化  检查应用版本  检查当前应用程序是否合法
@@ -19,7 +30,46 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.activity_splash);
         tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
         tv_splash_version.setText("版本号:"+getVersionName());
+        //检查升级
+        checkupdate();
     }
+
+    /**
+     * 检查是否有新版本，如果有就升级
+     */
+    private void checkupdate() {
+        //子线程请求网络
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+             //URL
+                String path=getString(R.string.serverurl); //请求的地址
+                try {
+                    URL url=new URL(path);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setReadTimeout(4000);
+                    int code = connection.getResponseCode();
+                    if(code==200){
+                        //联网成功
+                        InputStream is= connection.getInputStream();
+                        //把流转换成string
+                        String response = StreamTool.readFromStream(is).toString();
+                        Log.d("Message",response);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+        }).start();
+
+    }
+
+
+
     /**
      *得到应用程序的版本名
      */
---
+++ app/src/main/java/com/example/mobilesafe/com/example/mobilesafe/utils/StreamTool.java
@@ -0,0 +1,22 @@
+package com.example.mobilesafe.com.example.mobilesafe.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by abc on 2016/1/30.
+ */
+public class StreamTool {
+
+    public static  StringBuilder readFromStream(InputStream is) throws IOException {
+        BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+        if((line = bufferedReader.readLine())!=null){
+            response.append(line);
+        }
+        return response;
+    }
+}
