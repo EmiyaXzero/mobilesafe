3270738950b8aaef23a0f83f57353389f8cb1338
Emiya_zero <a825564615@gmail.com>
2016-02-06 13:17:09 +0800

    对查找进行优化，当输入框改变时自动查找数据库

--- app/src/main/java/com/example/mobilesafe/NumberAddressQueryActivity.java
+++ app/src/main/java/com/example/mobilesafe/NumberAddressQueryActivity.java
@@ -2,7 +2,9 @@ package com.example.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -24,6 +26,42 @@ public class NumberAddressQueryActivity extends Activity {
         setContentView(R.layout.activity_number_address_query);
         ed_phone = (EditText) findViewById(R.id.ed_phone);
         result = (TextView) findViewById(R.id.result);
+        ed_phone.addTextChangedListener(new TextWatcher() {
+            /**
+             *当文本发生变化之前回调
+             */
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            /**
+             * 当文本发生变化时回调
+             */
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+              if(s!=null&&s.length()>=3){
+                  //查询数据库并且显示结果
+                  find(s.toString());
+              }
+            }
+
+            /**
+             *当文本发生变化后回调
+             */
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void find(String s) {
+        //去数据库查询号码归属地
+        //查询数据库
+        String location=  NumberAddressQueryUtils.queryNumber(s);
+        result.setText(location);
     }
 
     /**
@@ -36,10 +74,9 @@ public class NumberAddressQueryActivity extends Activity {
             Toast.makeText(this, "号码为空", Toast.LENGTH_SHORT).show();
             return;
         }else {
-            //去数据库查询号码归属地
-            //查询数据库
-            String location=  NumberAddressQueryUtils.queryNumber(number);
-            result.setText(location);
+              find(number);
         }
     }
+
+
 }
