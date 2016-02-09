1f3d8ae5c922f0ac1becb9817696d3d992b3f5dd
Emiya_zero <a825564615@gmail.com>
2016-02-09 18:25:56 +0800

    优化ListView 利用ConverView和View.setTag进行优化

--- app/src/main/java/com/example/mobilesafe/CallSmsSafeActivity.java
+++ app/src/main/java/com/example/mobilesafe/CallSmsSafeActivity.java
@@ -2,6 +2,7 @@ package com.example.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -56,19 +57,42 @@ public class CallSmsSafeActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view=View.inflate(getApplicationContext(),R.layout.list_item_callsms,null);
-            TextView tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
-            TextView tv_black_mode = (TextView) view.findViewById(R.id.tv_black_mode);
+            View view;
+            ViewHolder holder;
+            //1.减少内存中view对象创建的次数
+            if(convertView==null){
+                //还没有产生多余的convertView
+                view=View.inflate(getApplicationContext(),R.layout.list_item_callsms,null);
+                //2.减少子查询的次数  内存对象的地址 记录对象的地址
+                holder=new ViewHolder();
+                holder.tv_number = (TextView) view.findViewById(R.id.tv_black_number);
+                holder.tv_mode = (TextView) view.findViewById(R.id.tv_black_mode);
+                //当孩子生出来时找到他们的引用，存放在记事本里，放在父亲的口袋里
+                view.setTag(holder);
+            }else{
+                view =convertView;
+                //只需要从口袋里拿出记事本即可
+                holder = (ViewHolder) view.getTag();
+            }
 
-            tv_black_number.setText(infos.get(position).getNumber());
+            holder.tv_number.setText(infos.get(position).getNumber());
             if("1".equals(infos.get(position).getMode())){ //避免空指针异常
-                tv_black_mode.setText("电话拦截");
+                holder.tv_mode.setText("电话拦截");
             }else if("2".equals(infos.get(position).getMode())){
-                tv_black_mode.setText("短信拦截");
+                holder.tv_mode.setText("短信拦截");
             }else if("3".equals(infos.get(position).getMode())){
-                tv_black_mode.setText("全部拦截");
+                holder.tv_mode.setText("全部拦截");
             }
             return view;
         }
+    }
+
+    /**
+     * View 对象的容器 记录孩子的内存地址
+     *相当于几十本
+     */
+    class ViewHolder{
+        TextView tv_number;
+        TextView tv_mode;
     }
 }
