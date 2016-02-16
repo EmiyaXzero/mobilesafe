ec041a1487c2ee49bf5bbb5aaf6f5c601d3d1ed2
Emiya_zero <a825564615@gmail.com>
2016-02-16 18:17:03 +0800

    完善看门狗细节

--- app/src/main/java/com/example/mobilesafe/Service/WatchDogService.java
+++ app/src/main/java/com/example/mobilesafe/Service/WatchDogService.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.example.mobilesafe.EnterPwdActivity;
 import com.example.mobilesafe.db.dao.ApplockDao;
@@ -28,42 +27,58 @@ public class WatchDogService extends Service {
     private InnerReceiver receiver;
 
     private ScreenoffReceiver screenoffReceiver;
+
+    private dataChangedReceiver dataChangedReceiver;
+
+    private List<String> protectPacknames;
+
+    private Intent intent;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+
     @Override
     public void onCreate() {
         flag=true;
         am= (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         dao=new ApplockDao(this);
+        protectPacknames=dao.findAll();
         receiver=new InnerReceiver();
         registerReceiver(receiver,new IntentFilter("com.example.mobilesafe.tempstop"));
         screenoffReceiver=new ScreenoffReceiver();
         registerReceiver(screenoffReceiver,new IntentFilter(Intent.ACTION_SCREEN_OFF));
+
+        dataChangedReceiver = new dataChangedReceiver();
+        registerReceiver(dataChangedReceiver,new IntentFilter("com.example.mobilesafe.applockchange"));
+        /**
+         * 意图可以先初始化，提高效率
+         */
+        intent = new Intent(getApplicationContext(), EnterPwdActivity.class);
+        //服务是没有栈信息的，在服务开启的activity的时候，要指定这个activity运行的任务栈
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         new Thread(){
             public void run(){
                 while(flag) {
-                    List<ActivityManager.RunningTaskInfo> infos =am.getRunningTasks(100);
+                    List<ActivityManager.RunningTaskInfo> infos =am.getRunningTasks(1);
                     String packName = infos.get(0).topActivity.getPackageName();
-                    Log.d("TGA",packName);
-                    if(dao.find(packName)){
+                    //if(dao.fin(packName))
+                    if(protectPacknames.contains(packName)){
+                        //查询数据库太慢 ,通过查询内存提高效率
                         if(packName.equals(tempStopProtectPackname)){
 
                         }else {
                             //当前应用需要保护。
-                            Intent intent = new Intent(getApplicationContext(), EnterPwdActivity.class);
-                            //服务是没有栈信息的，在服务开启的activity的时候，要指定这个activity运行的任务栈
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             //传递包名
                             intent.putExtra("packname", packName);
                             startActivity(intent);
                         }
                     }
                     try{
-                        Thread.sleep(50);
+                        Thread.sleep(20);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -79,8 +94,10 @@ public class WatchDogService extends Service {
         flag=false;
         unregisterReceiver(receiver);
         unregisterReceiver(screenoffReceiver);
+        unregisterReceiver(dataChangedReceiver);
         screenoffReceiver=null;
         receiver=null;
+        dataChangedReceiver=null;
         super.onDestroy();
     }
 
@@ -102,6 +119,12 @@ public class WatchDogService extends Service {
             tempStopProtectPackname=null;
         }
     }
+    private class dataChangedReceiver extends BroadcastReceiver{
 
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            protectPacknames=dao.findAll();
+        }
+    }
 }
 
--- app/src/main/java/com/example/mobilesafe/db/dao/ApplockDao.java
+++ app/src/main/java/com/example/mobilesafe/db/dao/ApplockDao.java
@@ -2,55 +2,83 @@ package com.example.mobilesafe.db.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.mobilesafe.db.ApplockDBOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by abc on 2016/2/16.
  * 程序锁的Dao
  */
 public class ApplockDao {
-     private ApplockDBOpenHelper helper;
+    private ApplockDBOpenHelper helper;
+
+    private Context context;
     /**
      * 构造方法
-     * @param context   上下文
+     *
+     * @param context 上下文
      */
-    public ApplockDao(Context context){
+    public ApplockDao(Context context) {
         helper = new ApplockDBOpenHelper(context);
+        this.context=context;
     }
+
     /**
      * 添加一个要锁定应用程序的包名
      */
-    public void add(String packname){
+    public void add(String packname) {
         SQLiteDatabase db = helper.getWritableDatabase();
-        ContentValues values =new ContentValues();
-        values.put("packname",packname);
-        db.insert("applock",null,values);
+        ContentValues values = new ContentValues();
+        values.put("packname", packname);
+        db.insert("applock", null, values);
         db.close();
+        Intent intent = new Intent();
+        intent.setAction("com.example.mobilesafe.applockchange");
+        context.sendBroadcast(intent);
     }
 
     /**
      * 删除一个要锁定应用程序的包名
      */
-    public void delete(String packname){
+    public void delete(String packname) {
         SQLiteDatabase db = helper.getWritableDatabase();
         db.delete("applock", "packname=?", new String[]{packname});
         db.close();
+        Intent intent = new Intent();
+        intent.setAction("com.example.mobilesafe.applockchange");
+        context.sendBroadcast(intent);
     }
 
     /**
      * 查询
      */
-    public boolean find(String packname){
-        boolean result=false;
+    public boolean find(String packname) {
+        boolean result = false;
         SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor cursor =db.query("applock", null, "packname=?", new String[]{packname}, null, null, null);
-        if(cursor.moveToNext()){
-            result= true;
+        Cursor cursor = db.query("applock", null, "packname=?", new String[]{packname}, null, null, null);
+        if (cursor.moveToNext()) {
+            result = true;
         }
+        cursor.close();
         db.close();
         return result;
+    }
+
+    public List<String> findAll() {
+        List<String> protectPacknames = new ArrayList<String>();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.query("applock", new String[]{"packname"}, null,null, null, null, null);
+        while(cursor.moveToNext()){
+            protectPacknames.add(cursor.getString(0));
+        }
+        cursor.close();
+        db.close();
+        return protectPacknames;
     }
 }
